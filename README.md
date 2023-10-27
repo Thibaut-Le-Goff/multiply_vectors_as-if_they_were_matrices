4e275dd95467de85ea0d605fcf27158742547b2c
Thibaut-Le-Goff <thibaut.le.goff27@gmail.com>
2023-10-27 23:02:02 +0200

    nouveau fichier : .gitignore 	modifié :         README.md 	nouveau fichier : do_not_work.png 	modifié :         multiply.rs

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
-$$\begin{bmatrix}
-1 & 2 & \\
-a & b & c
-\end{bmatrix} $$
+ <center><h2><ins>Multiply vectors as if they were matrices</ins></h2></center>
 
-$$
-\usepackage{tikz} 
-\tikzmark{c23}1$$
+The aim of this project is to be able to do the multiplication of two matrices, but from two vectors. 
+
+The motivation behind this is to make the gradient descent methods (from this project) work with matrices to train multiple neurons on the same layer.
+
+<h3><ins>The problem.</ins></h3>
+
+The function created takes two vectors as inputs :\
+vector1 = $\begin{bmatrix}
+0 & 1 & 2 & 3 & 4 & 5
+\end{bmatrix}$\
+vector2 = $\begin{bmatrix}
+0 & 1 & 2 & 3 & 4 & 5
+\end{bmatrix}$
+
+If we wanted to manipulate these vectors as matrices, we would have to rearrange them, but there are many ways of doing this:
+
+| | The possibilities :
+| --------------|---------------|
+| possibility1 = $\begin{matrix}[0 & 1 & 2 & 3 & 4 & 5]\end{matrix}$ | possibility2 = $\begin{matrix}[0 &1 & 2\\ 3 & 4 & 5]\end{matrix}$ |
+| possibility3 = $\begin{matrix}[0 & 1\\ 2 & 3\\ 4 & 5]\end{matrix}$ | possibility4 = $\begin{matrix}[0 \\ 1\\ 2 \\ 3\\ 4 \\ 5]\end{matrix}$ |
+
+We can't choose one of them without informations about the number of rows/columns we want for the result matrix.
+
+We, at least, need to either the rows or the columns of the matrix we would have as the result, this the third argument of the function.
+
+I raealised that the algorithme doesn't work with matrices of differente size :
+
+![Alt text](do_not_work.png)
+
+<h3><ins>Step 1 : Extract the rows from mat1</ins></h3>
+
+<h3><ins>Step 2 : Extract the collumns from mat1</ins></h3>
+<h3><ins>Step : </ins></h3>
+<h3><ins>Step : </ins></h3>
+<h3><ins>Step : </ins></h3>
+
+
+$$\begin{bmatrix}
+1 & 2 & 3\\
+a & b & c
+\end{bmatrix}$$
--- multiply.rs
+++ multiply.rs
@@ -4,39 +4,40 @@ fn mul(mat1: &[i32], mat2: &[i32], column_result: usize) {
 
     let divisor_mat2_for_row: usize = mat2.len() / column_result;
 
-    println!("divisor_mat2_for_row: {:?}", divisor_mat2_for_row);
+    println!("divisor_mat2_for_row : {:?}", divisor_mat2_for_row);
 
-    for col in 0..column_result {
-        // for each collumn of the result_mat
+    //for col in 0..column_result {
+    for row in 0..column_result {
+        // for each row of the result_mat
 
-        // extract the column from mat2
-        let mut vec_col: Vec<i32> = Vec::new();
- 
-        for col_mat2 in (col..mat2.len()).step_by(column_result) {
-            vec_col.push(mat2[col_mat2]);
+        // extract the row from mat1
+        let mut vec_row: Vec<i32> = Vec::new();
+
+        for row_mat1 in (row * divisor_mat2_for_row)..((row * divisor_mat2_for_row) + divisor_mat2_for_row) {
+            vec_row.push(mat1[row_mat1]);
         }
+        
+        println!("\nvec_row : {:?}", vec_row);
 
-        println!("\nvec_col: {:?}", vec_col);
 
+        //for row in 0..column_result {
+        for col in 0..column_result {
+            // for each collumn of the result_mat
 
-        for row in 0..column_result {
-            // for each row of the result_mat
-
-            // extract the row from mat1
-            let mut vec_row: Vec<i32> = Vec::new();
-
-            for row_mat1 in ((row * divisor_mat2_for_row)..((row * divisor_mat2_for_row) + divisor_mat2_for_row)) {
-                vec_row.push(mat1[row_mat1]);
+            // extract the column from mat2
+            let mut vec_col: Vec<i32> = Vec::new();
+    
+            for col_mat2 in (col..mat2.len()).step_by(column_result) {
+                vec_col.push(mat2[col_mat2]);
             }
 
-            println!("vec_row: {:?}", vec_row);
+            println!("vec_col : {:?}", vec_col);
 
-            
-            // multiply vec_col by vec_row
+            // multiply vec_row by vec_col
             let mut result: i32 = 0;
 
             for value in 0..vec_col.len() {
-                let pre_result: i32 = vec_col[value] * vec_row[value];
+                let pre_result: i32 = vec_row[value] * vec_col[value];
                 result += pre_result;
             }
 
@@ -44,14 +45,14 @@ fn mul(mat1: &[i32], mat2: &[i32], column_result: usize) {
         }
     }
 
-    println!("result_mat : {:?}", result_mat);
+    println!("\nresult_mat : {:?}", result_mat);
 }
 
 
 fn main() {
-    let mat1: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
-    let mat2: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
-    let column_result: usize = 2;
+    let mat1: Vec<i32> = vec![0, 1, 2, 3];
+    let mat2: Vec<i32> = vec![0, 1, 2, 3, 4, 5];
+    let column_result: usize = 3;
 
     mul(&mat1, &mat2, column_result);
 }
